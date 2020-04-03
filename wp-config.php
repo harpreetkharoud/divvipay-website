ecf4d6ec901ae852da5bf113f2fe16da9efabb4f
harpreet kharoud <harpreetkharoud90@gmail.com>
2020-04-02 22:03:06 -0700

    git remote add origin https://github.com/harpreetkharoud/divvipay-website.git

--- wp-config.php
+++ wp-config.php
@@ -50,17 +50,17 @@ define( 'DB_NAME', "divi" );
 
 /** MySQL database username */
 
-define( 'DB_USER', "root" );
+define( 'DB_USER', "divvipay" );
 
 
 /** MySQL database password */
 
-define( 'DB_PASSWORD', "root" );
+define( 'DB_PASSWORD', "divvipay12#" );
 
 
 /** MySQL hostname */
 
-define( 'DB_HOST', "localhost" );
+define( 'DB_HOST', "hiddengems.wmdd.ca" );
 
 
 /** Database Charset to use in creating database tables. */
